cd526039ef46a026108155e129e48e11300b8e0f
Victor <victor.garcia@tunedglobal.com>
2024-07-16 09:28:57 +0200

    PacemakerRadio -> 0.6.0

---
+++ builds/PacemakerRadio/0.6.0/PacemakerRadio.xcframework/ios-arm64/PacemakerRadio.framework/Headers/PacemakerRadio-umbrella.h
@@ -0,0 +1,22 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "automix-sdk/AutomixSDK.h"
+#import "radio-sdk/RadioSDKObjC.h"
+#import "automix-sdk/CoreDataModels/_AutomixSDKPlayLog.h"
+#import "automix-sdk/CoreDataModels/AutomixSDKPlayLog.h"
+#import "automix-sdk/CoreDataModels/_AutomixSDKTransitionLog.h"
+#import "automix-sdk/CoreDataModels/AutomixSDKTransitionLog.h"
+#import "automix-sdk/MDMCoreDataWrapper.h"
+
+FOUNDATION_EXPORT double PacemakerRadioVersionNumber;
+FOUNDATION_EXPORT const unsigned char PacemakerRadioVersionString[];
